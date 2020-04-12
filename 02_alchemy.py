0df1ccf9d823e00194f8dd9712a23f1ebf2c9c82
vm user <sysadm@carteblanche.ua>
2020-04-12 17:18:07 +0300

    алхимик+

--- 02_alchemy.py
+++ 02_alchemy.py
@@ -20,35 +20,111 @@
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
 
-class Storm:
-    def __init__(self):
-        print('Образовался шторм')
+class Lava:
+    def __str__(self):
+        return 'Лава'
 
+
+class Lightning:
+    def __str__(self):
+        return 'Молния'
+
+
+class Dust:
+    def __str__(self):
+        return 'Пыль'
+
+
+class Earth:
+    def __str__(self):
+        return 'Земля'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Mud()
+        elif isinstance(other, Fire):
+            return Lava()
+        elif isinstance(other, Air):
+            return Dust()
+        else:
+            return None
+
+
+class Mud:
+    def __str__(self):
+        return 'Грязь'
+
+
+class Steam:
+    def __str__(self):
+        return 'Пар'
+
+
+class Fire:
+    def __str__(self):
+        return 'Огонь'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Lava()
+        elif isinstance(other, Air):
+            return Lightning()
+        else:
+            return None
+
+
+class Storm:
     def __str__(self):
         return 'Штром'
 
 
 class Air:
-    def __init__(self):
-        print('Появился воздух')
-
     def __str__(self):
         return 'Воздух'
 
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Earth):
+            return Dust()
+        elif isinstance(other, Water):
+            return Storm()
+        else:
+            return None
+
 
 class Water:
-    def __init__(self):
-        print('Вода появилась')
-
     def __str__(self):
         return 'Вода'
 
     def __add__(self, other):
         if isinstance(other, Air):
             return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Mud()
+        else:
+            return None
 
 
-print(Water(), '+', Air(), '=', Water() + Air())
+print(Water(), '+', Air(), Water()+Air())
+print(Water(), '+', Fire(), Water()+Fire())
+print(Water(), '+', Earth(), Water()+Earth())
+
+print(Air(), '+', Fire(), Air()+Fire())
+print(Air(), '+', Earth(), Air()+Earth())
+print(Fire(), '+', Earth(), Fire()+Earth())
+
+#   Вода + Воздух = Шторм
+#   Вода + Огонь = Пар
+#   Вода + Земля = Грязь
+#   Воздух + Огонь = Молния
+#   Воздух + Земля = Пыль
+#   Огонь + Земля = Лава
+
 
 
 
